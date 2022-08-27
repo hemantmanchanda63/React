829d77753e93f4ee825adc92159ef484c9eb5526
Himanshu Manchanda <41925126+hemantmanchanda63@users.noreply.github.com>
2022-08-27 17:46:15 +0530

    Updates

---
+++ precache-manifest.6461c2fdfce8e1ea98676fcc985d01f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa457cf723aab5e2265e",
+    "url": "/Text-Utility/static/css/main.f04b8fe7.chunk.css"
+  },
+  {
+    "revision": "fa457cf723aab5e2265e",
+    "url": "/Text-Utility/static/js/main.fa457cf7.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Text-Utility/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Text-Utility/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "9659b02077fffd26b7be",
+    "url": "/Text-Utility/static/js/runtime~main.9659b020.js"
+  },
+  {
+    "revision": "6d7d885ac53422c78656c91b72a06180",
+    "url": "/Text-Utility/index.html"
+  }
+];
